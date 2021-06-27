852e28427f1d1241411cb5238cfe417eec183c04
cpu_1 <74270643+ChervyachokMigo@users.noreply.github.com>
2021-06-28 03:22:17 +0500

    Add files via upload

--- ban_not_utorrent.cmd
+++ ban_not_utorrent.cmd
@@ -1,5 +1,6 @@
+@echo off
 chcp 1251
-cd "C:\Users\Администратор\Desktop\btt\uTorrentBlockXunlei\ban_not_bt\"
-nodejs\node.exe utorrent.js
+cd "C:\Users\Администратор\Desktop\btt\uTorrentBlockXunlei\ban_not_bt\nodejs\"
+node.exe utorrent.js
 
 pause
